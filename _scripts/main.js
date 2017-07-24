6bc9df860593f874b96f831fd04e0a1aeacf512b
Brittany Chiang <brittany.chiang@gmail.com>
2017-07-24 09:55:54 -0700

    you dont need jquery

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,22 +1,20 @@
-$(function() {
+const d = new Date();
+const hours = d.getHours();
+const nighttime = ( hours >= 19 )  || ( hours <= 7 ); // 7pm <= hours <= 7am
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+const input = document.getElementById('switch');
 
-  const d = new Date();
-  const hours = d.getHours();
-  // after 7pm or before 7am
-  const nighttime = ( hours >= 19 )  || ( hours <= 7 );
+if ( nighttime ) {
+  input.checked = true;
+  body.classList.add('night');
+}
 
-  if ( nighttime ) {
-    $('#switch').prop( 'checked', true );
-    $('body').addClass('night');
+toggle.addEventListener( 'click', function() {
+  const isChecked = input.checked;
+  if ( isChecked ) {
+    body.classList.remove('night');
+  } else {
+    body.classList.add('night');
   }
-
-  $('#toggle').click(function() {
-    const isChecked = $('#switch').is(':checked');
-    if ( isChecked ) {
-      $('body').removeClass('night');
-    } else {
-      $('body').addClass('night');
-    }
-  });
-
 });
