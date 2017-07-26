01431fa10c2260076036dfb038e406a930fcab38
Brittany Chiang <brittany.chiang@gmail.com>
2017-07-26 14:37:42 -0700

    top button

--- _scripts/main.js
+++ _scripts/main.js
@@ -18,3 +18,22 @@ toggle.addEventListener( 'click', function() {
     body.classList.add('night');
   }
 });
+
+
+const introHeight = document.getElementById('intro').offsetHeight;
+const topButton = document.getElementById('top-button');
+const $topButton = $('#top-button');
+
+window.addEventListener( 'scroll', function() {
+  if ( window.scrollY > introHeight ) {
+    $topButton.fadeIn();
+  } else {
+    $topButton.fadeOut();
+  }
+}, false);
+
+topButton.addEventListener( 'click', function() {
+  $("html, body").animate({
+    scrollTop: 0
+  }, 600);
+});
