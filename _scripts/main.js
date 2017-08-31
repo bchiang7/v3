5449d5e957b3087695841add8604b0b21d2f0de3
Brittany Chiang <brittany.chiang@gmail.com>
2017-08-31 16:38:14 -0700

    scroll reveal

--- _scripts/main.js
+++ _scripts/main.js
@@ -21,6 +21,7 @@ $(function() {
     }
   });
 
+
   const introHeight = document.querySelector('.intro').offsetHeight;
   const topButton = document.getElementById('top-button');
   const $topButton = $('#top-button');
@@ -34,10 +35,21 @@ $(function() {
   }, false);
 
   topButton.addEventListener('click', function() {
-    $('html, body').animate({ scrollTop: 0 }, 500);
+    $('html, body').animate({scrollTop: 0}, 500);
   });
 
+
+
   const hand = document.querySelector('.emoji.wave-hand');
+
+  function waveOnLoad() {
+    hand.classList.add('wave');
+    setTimeout(function() {
+      hand.classList.remove('wave');
+    }, 2000);
+  }
+
+  setTimeout(function() { waveOnLoad(); }, 1000);
 
   hand.addEventListener('mouseover', function() {
     hand.classList.add('wave');
@@ -46,5 +58,20 @@ $(function() {
   hand.addEventListener('mouseout', function() {
     hand.classList.remove('wave');
   });
+
+
+  window.sr = ScrollReveal({
+    reset: true,
+    duration: 600,
+    easing: 'cubic-bezier(.694,0,.335,1)',
+    scale: 1,
+    viewFactor: 0.3
+  });
+
+  sr.reveal('.background');
+  sr.reveal('.skills');
+  sr.reveal('.experience', {viewFactor: 0.2});
+  sr.reveal('.featured-projects', {viewFactor: 0.1});
+  sr.reveal('.other-projects', {viewFactor: 0.05});
 
 });
